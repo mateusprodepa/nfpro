999289ab812076ffe7a175bd2876f8f9d0b4a25e
Common <mateus7319@gmail.com>
2018-06-24 21:44:37 -0300

    deploy

--- public/App.js
+++ public/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import Login from './components/login/login';
 import Cadastro from './components/cadastro/cadastro.js';
+import { createStackNavigator } from 'react-navigation';
 import { View, Text } from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
-      <Cadastro />
+      <Login />
     );
   }
 }
+
+const AppStackNavigator = createStackNavigator({
+  Login,
+});
